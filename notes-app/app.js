d517de658ff4f9617ea7ecaa539a308b53ef942a
Simon Hirst <simon.hirst@dootrix.com>
2020-05-21 15:47:18 +0100

    (4.15): getting input from users

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,7 +1,12 @@
 const getNotes = require('./notes')
 const chalk = require('chalk')
 
-console.log(getNotes())
+const command = process.argv[2]
 
-const log = console.log;
-log(chalk.red.bold.inverse("Error!"))
+console.log(process.argv)
+
+if(command === 'add'){
+    console.log('adding note')
+} else if(command === 'remove'){
+    console.log('removing note')
+}
