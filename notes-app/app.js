dd200d4f1752000e656f434f841cd16f8485ec6e
Simon Hirst <simon.hirst@dootrix.com>
2020-05-21 16:51:56 +0100

    (4.17): argument parsing with yargs: part ii

--- notes-app/app.js
+++ notes-app/app.js
@@ -9,8 +9,20 @@ yargs.version('1.1.0')
 yargs.command({
     command: 'add',
     describe: 'Adds a new note',
-    handler: function (){
-        console.log('adding a new note')
+    builder:{
+        title:{
+            describe: 'Note title',
+            demandOption: true,
+            type: 'string'
+        },
+        body:{
+            describe: 'Note body',
+            demandOption: true,
+            type: 'string'
+        }
+    },
+    handler: function (argv){
+        console.log('title: ' + argv.title + ' body: ' + argv.body)
     }
 })
 
@@ -41,4 +53,4 @@ yargs.command({
     }
 })
 
-console.log(yargs.argv)
+yargs.parse()
