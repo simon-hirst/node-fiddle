78f2c8d3afcdd3e9bef2079c3e9c790bbab31810
Simon Hirst <simonhirst@pm.me>
2020-05-22 18:12:13 +0100

    (4.19): adding a note

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,42 @@
+const fs = require('fs');
+
 const getNotes = function () {
   return 'Your notes...';
 };
 
-module.exports = getNotes;
+const addNote = function (title, body) {
+  const notes = loadNotes();
+  const duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push({
+      title,
+      body,
+    });
+
+    saveNotes(notes);
+    console.log('new note added');
+  } else {
+    console.log('note title taken already');
+  }
+};
+
+const saveNotes = function (notes) {
+  const dataJSON = JSON.stringify(notes);
+  fs.writeFileSync('notes.json', dataJSON);
+};
+
+const loadNotes = function () {
+  try {
+    const dataBuffer = fs.readFileSync('notes.json');
+    const dataJSON = dataBuffer.toString();
+    return JSON.parse(dataJSON);
+  } catch (e) {
+    return [];
+  }
+};
+
+module.exports = {
+  getNotes,
+  addNote,
+};
