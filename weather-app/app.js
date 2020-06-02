4636ab1c8a0a7764d8c97110c8c501b0b8edde77
Simon Hirst <simon.hirst@dootrix.com>
2020-06-02 15:36:39 +0100

    (6.32): customising http requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -2,7 +2,8 @@ const request = require('postman-request')
 
 const url = "http://api.weatherstack.com/current?access_key=f8c74517cbd323edbb550f9b4b221da5&query=37.8267,-122.4233"
 
-request(url, (error, response) => {
-    const data = JSON.parse(response.body)
-    console.log(data.current)
+request({url: url, json: true}, (error, response) => {
+    console.log(response.body.current.weather_descriptions[0] + 
+        `. It is currently ${response.body.current.temperature}c with a` +
+        ` ${response.body.current.precip}% chance of rain.`)
 })
