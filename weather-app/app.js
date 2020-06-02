a61b5c22db0898357d537a7da6d65cb5be764686
Simon Hirst <simon.hirst@dootrix.com>
2020-06-02 11:53:43 +0100

    (6.31): making http requests

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,8 @@
-console.log('Starting')
+const request = require('postman-request')
 
-setTimeout(() => {
-    console.log('2 second timer')
-}, 2000)
+const url = "http://api.weatherstack.com/current?access_key=f8c74517cbd323edbb550f9b4b221da5&query=37.8267,-122.4233"
 
-setTimeout(() => {
-    console.log('0 second timer')
-}, 0)
-
-console.log('Stopping')
+request(url, (error, response) => {
+    const data = JSON.parse(response.body)
+    console.log(data.current)
+})
